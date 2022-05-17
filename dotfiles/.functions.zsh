f0d49887480fb0f04d29faa5492f55dc9af843af
Michael Bottens <michael.bottens@shopify.com>
2022-05-17 16:23:10 -0400

    updating rubymine configuration

--- dotfiles/.functions.zsh
+++ dotfiles/.functions.zsh
@@ -1,15 +1,11 @@
 download_latest_rubymine() {
   echo "Installing latest RubyMine"
-  echo "This doesn't currently work, check in on https://youtrack.jetbrains.com/issue/CWM-4358 periodically"
 
-  # also interesting: https://youtrack.jetbrains.com/issue/GTW-782
-  if [[ $FORCE_RUBYMINE_INSTALL ]]; then
-    RM_VERSION_LINK=$(curl -s https://data.services.jetbrains.com/products\?code\=RM\&release.type\=eap%2Crc%2Crelease\&fields\=distributions%2Clink%2Cname%2Creleases | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['releases'][0]['downloads']['linux']['link'])")
-    mkdir -p /home/spin/.cache/JetBrains/RemoteDev/dist
-    curl -L $RM_VERSION_LINK --output /tmp/rubymine.tar.gz
-    tar -zxf /tmp/rubymine.tar.gz -C /home/spin/.cache/JetBrains/RemoteDev/dist
-    touch /home/spin/.cache/JetBrains/RemoteDev/dist/latest/.expandSucceeded
-  fi
+  RM_VERSION_LINK=$(curl -s https://data.services.jetbrains.com/products\?code\=RM\&release.type\=eap%2Crc%2Crelease\&fields\=distributions%2Clink%2Cname%2Creleases | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['releases'][0]['downloads']['linux']['link'])")
+  mkdir -p /home/spin/.cache/rubymine
+  curl -L $RM_VERSION_LINK --output /tmp/rubymine.tar.gz
+  tar -zxf /tmp/rubymine.tar.gz -C /home/spin/.cache/rubymine
+  */bin/remote-dev-server.sh registerBackendLocationForGateway
 }
 
 install_latest_rubymine() {
@@ -23,7 +19,7 @@ install_latest_rubymine() {
 
 if [[ $SPIN ]]; then
   # todo: latest needs to be replaced with version of extracted rubymine archive, can get this value from curl request
-  if [[ ! -d /home/spin/.cache/JetBrains/RemoteDev/dist/latest/.expandSucceeded ]]; then
+  if [[ ! -d ~/home/spin/.cache/rubymine ]]; then
     install_latest_rubymine
   fi
 fi
